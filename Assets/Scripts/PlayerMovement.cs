73ba588eff96cbbdba1e76e0caeb705c31b8f24c
Zach Martin <zach@zach-martin.com>
2022-04-07 01:51:24 -0400

    Added initial movement functionality

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed = 5f;
+
+    private Vector2 _moveInput;
+    private Rigidbody2D _rigidbody;
+
+    private void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Run();
+    }
+
+    private void OnMove(InputValue value)
+    {
+        _moveInput = value.Get<Vector2>();
+        Debug.Log(_moveInput);
+    }
+
+    private void Run()
+    {
+        Vector2 playerVelocity = new Vector2(_moveInput.x * speed, _rigidbody.velocity.y);
+        _rigidbody.velocity = playerVelocity;
     }
 }
